8e0d14d4dedde0d7234d4a9a8e2df8ecc43c2c50
elijah <cuiyeqing960904@gmail.com>
2023-11-09 14:07:10 -0500

    new customer aggregate

--- aggregate/customer.go
+++ aggregate/customer.go
@@ -1,1 +1,36 @@
 package aggregate
+
+import (
+	"ddd-template/entity"
+	"ddd-template/valueobject"
+	"errors"
+
+	"github.com/google/uuid"
+)
+
+var (
+	ErrInvalidPerson = errors.New("a customer has to have an valid person")
+)
+
+type Customer struct {
+	person       *entity.Person
+	products     []*entity.Item
+	transactions []valueobject.Transaction
+}
+
+func NewCustomer(name string) (Customer, error) {
+	if name == "" {
+		return Customer{}, ErrInvalidPerson
+	}
+
+	person := &entity.Person{
+		Name: name,
+		ID:   uuid.New(),
+	}
+
+	return Customer{
+		person:       person,
+		products:     make([]*entity.Item, 0),
+		transactions: make([]valueobject.Transaction, 0),
+	}, nil
+}
--- entity/person.go
+++ entity/person.go
@@ -4,8 +4,6 @@ import "github.com/google/uuid"
 
 type Person struct {
 	ID uuid.UUID
-	
 	Name string
-	
 	Age int
 }
