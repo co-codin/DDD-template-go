354fab40e0aa1cdc46167a1a19ac4dce4c486786
elijah <cuiyeqing960904@gmail.com>
2023-11-09 14:12:35 -0500

    building customer test

---
+++ aggregate/customer_test.go
@@ -0,0 +1,33 @@
+package aggregate_test
+
+import (
+	"ddd-template/aggregate"
+	"testing"
+)
+
+func TestCustomer_NewCustomer(t *testing.T) {
+	type testCase struct {
+		test        string
+		name        string
+		expectedErr error
+	}
+
+	testCases := []testCase{
+		{
+			test:        "Empty Name validation",
+			name:        "",
+			expectedErr: aggregate.ErrInvalidPerson,
+		},
+		{
+			test:        "Valid Name",
+			name:        "Percy Bolmer",
+			expectedErr: nil,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.test, func(t *testing.T) {
+			
+		})
+	}
+}
