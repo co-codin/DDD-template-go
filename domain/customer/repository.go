05eb4a35faa341f8ea2aeb92421defa21f97c0f6
elijah <cuiyeqing960904@gmail.com>
2023-11-13 12:45:54 -0500

    customer repo

--- domain/customer/repository.go
+++ domain/customer/repository.go
@@ -1,1 +1,23 @@
 package customer
+
+import (
+	"ddd-template/aggregate"
+	"errors"
+
+	"github.com/google/uuid"
+)
+
+var (
+	// ErrCustomerNotFound is returned when a customer is not found.
+	ErrCustomerNotFound = errors.New("the customer was not found in the repository")
+	// ErrFailedToAddCustomer is returned when the customer could not be added to the repository.
+	ErrFailedToAddCustomer = errors.New("failed to add the customer to the repository")
+	// ErrUpdateCustomer is returned when the customer could not be updated in the repository.
+	ErrUpdateCustomer = errors.New("failed to update the customer in the repository")
+)
+
+type CustomerRepository interface {
+	Get(uuid.UUID) (aggregate.Customer, error)
+	Add(aggregate.Customer) error
+	Update(aggregate.Customer) error
+}
